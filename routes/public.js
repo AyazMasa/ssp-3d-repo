7756041f6dd4bca0a76500a173dc0a616b6fbd11
AyazMasa <ayazmasa123@gmail.com>
2026-03-06 21:51:59 +0200

    Fix contact form: use standard POST with _next redirect instead of AJAX (Formspree reCAPTCHA blocks AJAX)

--- routes/public.js
+++ routes/public.js
@@ -3,6 +3,11 @@ const router = express.Router();
 
 router.get(["/", "/home"], (req, res) => res.render("home", { title: "Home" }));
 router.get("/about", (req, res) => res.render("about", { title: "About" }));
-router.get("/contact", (req, res) => res.render("contact", { title: "Contact" }));
+router.get("/contact", (req, res) => {
+  const protocol = req.protocol;
+  const host = req.get('host');
+  const siteUrl = `${protocol}://${host}`;
+  res.render("contact", { title: "Contact", sent: req.query.sent === '1', siteUrl });
+});
 
 module.exports = router;
